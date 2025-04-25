57bd88c91bf24db956a26f44ef13afb91a6172b7
mbmb-mbmb <markus.bonsdorff@iki.fi>
2025-04-25 15:53:38 +0300

    multiple PIPES seem to work.

--- exec_pipe.c
+++ exec_pipe.c
@@ -6,15 +6,29 @@ void	exec_pipe(t_ast *node, t_arena *env_arena, t_exec_status *exec_status)
 	pid_t	pidL;
 	pid_t	pidR;
 
-	pipe(pipe_fd);
+	if (pipe(pipe_fd) == -1)
+	{
+		perror("pipe");
+		return;
+	}
 	pidL = fork();
+	if (pidL == -1)
+	{
+		perror("fork");
+		close(pipe_fd[0]);
+		close(pipe_fd[1]);
+		return;
+	}
 	if (pidL == 0)
 	{
 		close(pipe_fd[0]);
 		dup2(pipe_fd[1], STDOUT_FILENO);
 		close(pipe_fd[1]);
-		execute_command(node->left, env_arena, exec_status);
-		exit(0);
+		if (node->left->type == PIPE)
+			exec_pipe(node->left, env_arena, exec_status);
+		else
+			execute_command(node->left, env_arena, exec_status);
+		exit(exec_status->exit_code);
 	}
 	pidR = fork();
 	if (pidR == 0)
@@ -23,7 +37,7 @@ void	exec_pipe(t_ast *node, t_arena *env_arena, t_exec_status *exec_status)
 		dup2(pipe_fd[0], STDIN_FILENO);
 		close(pipe_fd[0]);
 		execute_command(node->right, env_arena, exec_status);
-		exit(0);
+		exit(exec_status->exit_code);
 	}
 	close(pipe_fd[0]);
 	close(pipe_fd[1]);
