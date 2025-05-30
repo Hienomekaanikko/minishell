ad07a42af0eef8021ecef663889dd11be7f6c250
mbmb-mbmb <markus.bonsdorff@iki.fi>
2025-05-30 22:01:00 +0300

    signal error handler only status input

--- exec_error.c
+++ exec_error.c
@@ -38,28 +38,27 @@ int	error_handler(t_exec_status *status, const char *cmd, const char *msg, int e
 	return (exit_code);
 }
 
-void	handle_signal_error(t_exec_status *status, int signal)
+void	handle_signal_error(t_exec_status *status)
 {
-	status->signal = signal;
-	status->exit_code = 128 + signal;
+	status->exit_code = 128 + status->signal;
 	ft_putstr_fd("minishell: ", 2);
-	if (signal == SIGINT)
+	if (status->signal == SIGINT)
 		ft_putstr_fd("\n", 2);
-	else if (signal == SIGSEGV)
+	else if (status->signal == SIGSEGV)
 		ft_putstr_fd("Segmentation fault", 2);
-	else if (signal == SIGBUS)
+	else if (status->signal == SIGBUS)
 		ft_putstr_fd("Bus error", 2);
-	else if (signal == SIGFPE)
+	else if (status->signal == SIGFPE)
 		ft_putstr_fd("Floating point exception", 2);
-	else if (signal == SIGILL)
+	else if (status->signal == SIGILL)
 		ft_putstr_fd("Illegal instruction", 2);
-	else if (signal == SIGTERM)
+	else if (status->signal == SIGTERM)
 		ft_putstr_fd("Terminated", 2);
-	else if (signal == SIGKILL)
+	else if (status->signal == SIGKILL)
 		ft_putstr_fd("Killed", 2);
-	else if (signal == SIGPIPE)
+	else if (status->signal == SIGPIPE)
 		ft_putstr_fd("Broken pipe", 2);
-	else if (signal == SIGQUIT)
+	else if (status->signal == SIGQUIT)
 	{
 		if (WCOREDUMP(status->raw_status))
 			ft_putstr_fd("Quit (core dumped)\n", 2);
