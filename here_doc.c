3a0466604110a8d539f76321ca8de6e18eadd8c3
Markus Bonsdorff <mbonsdor@c2r2p10.hive.fi>
2025-06-04 11:19:30 +0300

    refactored HEREDOC. free_exp_tools() added

--- here_doc.c
+++ here_doc.c
@@ -3,109 +3,154 @@
 /*                                                        :::      ::::::::   */
 /*   here_doc.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msuokas <msuokas@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: mbonsdor <mbonsdor@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 10:18:54 by msuokas           #+#    #+#             */
-/*   Updated: 2025/06/04 09:47:51 by msuokas          ###   ########.fr       */
+/*   Updated: 2025/06/04 11:16:17 by mbonsdor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-#include "minishell.h"
 
-char	*make_filename(t_data *data)
+char	*make_temp_file_name(t_data *data)
 {
 	static int	counter = 0;
 	char		*num;
-	char		*filename;
+	char		*temp_file_name;
 
 	num = ft_itoa(counter++);
 	if (set_mem_error(data, num))
 		return (NULL);
-	filename = ft_strdup("./heredoc_");
-	if (set_mem_error(data, filename))
+	temp_file_name = ft_strdup("./heredoc_");
+	if (set_mem_error(data, temp_file_name))
 		return (NULL);
-	filename = ft_strjoin_free(filename, num);
-	if (set_mem_error(data, filename))
+	temp_file_name = ft_strjoin_free(temp_file_name, num);
+	if (set_mem_error(data, temp_file_name))
 	{
 		free(num);
 		return (NULL);
 	}
 	free(num);
-	return (filename);
+	return (temp_file_name);
+}
+
+int	hd_interrupt_eof(t_data *data, char *line, int linecount, char *delimiter)
+{
+	if (g_interrupted)
+	{
+		data->redir_err = 2;
+		g_interrupted = 0;
+		free(line);
+		return (1);
+	}
+	else if (!line)
+	{
+		ft_putstr_fd("minishell: warning: here-document at line ", 2);
+		ft_putnbr_fd(linecount, 2);
+		ft_putstr_fd(" delimited by end-of-file (wanted `", 2);
+		ft_putstr_fd(delimiter, 2);
+		ft_putstr_fd("')\n", 2);
+		return (1);
+	}
+	return (0);
+}
+
+char	*hd_expand_line(t_data *data, char **line, int is_delim_quote)
+{
+	char	*expanded_line;
+
+	if (ft_strchr(*line, '$') && !is_delim_quote)
+	{
+		expanded_line = expand(data, data->tools, *line);
+		if (expanded_line)
+		{
+			free(*line);
+			*line = ft_strdup(expanded_line);
+			free_exp_tools(data);
+			free(expanded_line);
+		}
+	}
+	return (*line);
+}
+
+int	hd_handle_delimiter(char *line, char *delimiter)
+{
+	if (ft_strncmp(line, delimiter, ft_strlen(delimiter)) == 0)
+	{
+		free(line);
+		return (1);
+	}
+	return (0);
+}
+
+int	hd_file_setup(t_data *data, char **out_path)
+{
+	int		fd;
+	char	*temp_file_name;
+
+	temp_file_name = make_temp_file_name(data);
+	if (!temp_file_name)
+		return (-1);
+	fd = open(temp_file_name, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd < 0)
+	{
+		ft_putstr_fd("error: cannot create heredoc file\n", 2);
+		free(temp_file_name);
+		return (-1);
+	}
+	*out_path = temp_file_name;
+	return (fd);
+}
+
+int	handle_delim_quote(char *delimiter)
+{
+	if (delimiter[0] == '\'' || delimiter[0] == '"')
+	{
+		delimiter = remove_quotes(delimiter);
+		return (1);
+	}
+	return (0);
+}
+
+void	hd_write_line(int fd, char *line)
+{
+	write(fd, line, ft_strlen(line));
+	write(fd, "\n", 1);
+	free(line);
+}
+
+int	redir_error_check(t_data *data)
+{
+	if (data->redir_err == 2)
+		return (-1);
+	return (0);
 }
 
 int	write_heredoc(t_data *data, char *delimiter, char **out_path)
 {
-	char	*filename;
 	char	*line;
-	char	*expanded_line;
-	int		delim_quote;
+	int		is_delim_quote;
 	int		fd;
 	int		linecount;
 
-	delim_quote = 0;
-	if (delimiter[0] == '\'' || delimiter[0] == '"')
-	{
-		delim_quote = 1;
-		delimiter = remove_quotes(delimiter);
-	}
+	is_delim_quote = handle_delim_quote(delimiter);
 	setup_heredoc_signals();
-	filename = make_filename(data);
-	if (!filename)
-		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = hd_file_setup(data, out_path);
 	if (fd < 0)
-	{
-		ft_putstr_fd("error: cannot create heredoc file\n", 2);
-		free(filename);
-		return (-1);
-	}
+		return (-1); //what should i return here?
 	linecount = 0;
 	while (1)
 	{
 		line = readline("> ");
-		if  (g_interrupted)
-		{
-			//cleanup here
-			data->redir_err = 2;
-			g_interrupted = 0;
-			free(line);
+		if (hd_interrupt_eof(data, line, linecount, delimiter))
 			break ;
-		}
-		if (!line)
-		{
-			ft_putstr_fd("minishell: warning: here-document at line ", 2);
-			ft_putnbr_fd(linecount, 2);
-			ft_putstr_fd(" delimited by end-of-file (wanted `", 2);
-			ft_putstr_fd(delimiter, 2);
-			ft_putstr_fd("')\n", 2);
-			break ;
-		}
 		linecount++;
-		if (ft_strchr(line, '$') && !delim_quote)
-		{
-			expanded_line = expand(data, data->tools, line);
-			if (expanded_line)
-			{
-				free(line);
-				line = ft_strdup(expanded_line);
-				free_exp_tools(data);
-			}
-		}
-		if (ft_strncmp(line, delimiter, ft_strlen(delimiter)) == 0)
-		{
-			free(line);
+		line = hd_expand_line(data, &line, is_delim_quote);
+		if (hd_handle_delimiter(line, delimiter))
 			break ;
-		}
-		write(fd, line, ft_strlen(line));
-		write(fd, "\n", 1);
-		free(line);
+		hd_write_line(fd, line);
 	}
 	close(fd);
 	setup_shell_signals();
-	*out_path = filename;
-	if (data->redir_err == 2)
-		return (-1);
-	return (0);
+	return (redir_error_check(data));
 }
